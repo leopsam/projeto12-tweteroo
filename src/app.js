437ec8b771fb20bc564363f486d4a88367ff95ed
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-01-08 02:54:19 -0300

    sign-up

--- src/app.js
+++ src/app.js
@@ -1,22 +1,49 @@
-import express from 'express';
-import cors from 'cors'; 
+import express from 'express'
+import cors from 'cors' 
 
-const user1 = {
-	username: 'leonardo2015', 
-	avatar: "https://i1.sndcdn.com/avatars-1zHN7xdLem5rBIJu-NmQ79g-t500x500.jpg" 
-}
+const users = [
+	{
+		username: 'bobesponja', 
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+	}
+]
 
-const app = express();
-app.use(cors()); 
+const tweets = []
 
-app.get("/teste", (req, res) => {
-    res.send("Hello World");
-});
+//--------------------------------------------------------
 
-app.get("/teste2", (req, res) => {
-    res.send('foi');
-});
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+app.post('/sign-up', (req, res) => {
+	const user = req.body
+	users.push(user)
+  	res.send("OK")
+})
+
+//--------------------------------------------------------
+
+app.post('/tweets', (req, res) => {
+	const newTweet = req.body
+
+	/*if(!users.username){
+		return res.sendStatus(401)
+	}*/
+	
+	tweets.push(newTweet)
+  	res.send(newTweet)
+})
+
+//--------------------------------------------------------
+
+app.get("/tweets", (req, res) => {
+	
+	
+})
+
+//--------------------------------------------------------
 
 app.listen(5000, () => {
 	console.log("Servidor rodou de boas!")
-});
+})
